5d8b83b9afa89400f1df822407d24ccbc1370551
YanShuai-Dilato <shuai.yan@dilatoit.com>
2013-03-06 18:18:54 +0800

    Add testapi_unlock.js
    Test cases on unlocking signing keys.

--- main/testcases/testapi_unlock.js
+++ main/testcases/testapi_unlock.js
@@ -3,9 +3,7 @@ var data = require('./testapi_data.json');
 var options_ios = {
     form: {
         data: {
-            cert_passwd: "Global1",
-            locked: "false",
-            default: "true"
+            password: "Global1"
         }
     }
 };
@@ -13,10 +11,8 @@ var options_ios = {
 var options_android = {
     form: {
         data: {
-            cert_password: "d!lat0",
-            keystore_password: "d!lat0",
-            locked: "false",
-            default: "true"
+            key_pw: "d!lat0",
+            keystore_pw: "d!lat0"
         }
     }
 };
@@ -24,29 +20,31 @@ var options_android = {
 var options_blackberry = {
     form: {
         data: {
-            key_password: "dilat0",
-            locked: "false",
-            default: "true"
+            password: "dilat0"
         }
     }
 };
 
 var runcase = function(api){
 
+    // Good
     api.put('/keys/ios/54642', options_ios, function(e, data) {
         console.log('ios error:', e);
         console.log('ios data: ', data);
     });
 
+    // Good
     api.put('/keys/android/13390', options_android, function(e, data) {
         console.log('android error:', e);
         console.log('android data:', data);
     });    
 
+    // Good
     api.put('/keys/blackberry/3366', options_blackberry, function(e, data) {
         console.log('bb error:', e);
         console.log('bb data:', data);
     });
+
 }
 
 module.exports.runcase = runcase;
